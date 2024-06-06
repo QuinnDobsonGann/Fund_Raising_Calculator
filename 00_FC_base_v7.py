b2488cff3cb7300ccbc3987bb1fcb17e19bd9a96
Quinn Dobson_Gann <134651936+QuinnDobsonGann@users.noreply.github.com>
2024-06-06 12:17:11 +1200

    Add files via upload

--- 00_FC_base_v7.py
+++ 00_FC_base_v7.py
@@ -20,3 +20,23 @@ def num_check(question, error, num_type):
 
         except ValueError:
             print(error)
+
+
+# Checks that user has entered yes / no to a question
+def yes_no(question):
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
+
+# **** Main Routine goes here ****
